69a58060e9f3550788fc23af7a9505d45f5830ba
Charles J Perry <cjperry85@outlook.com>
2018-07-25 06:14:27 -0400

    Add files via upload

---
+++ recursion.py
@@ -0,0 +1,6 @@
+# iterating through a list and printing the elements recursively
+def num_list(numbers):
+	for index in range(0,len(numbers)):
+		return numbers[index:]
+
+print(num_list([100, 20, 18, 12, 5, 5]))
