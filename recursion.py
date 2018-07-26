4fba8baa1ebbf9cc87e24e0633a0e81e71dcc961
Charles J Perry <cjperry85@outlook.com>
2018-07-26 16:38:00 -0400

    Update recursion.py

--- recursion.py
+++ recursion.py
@@ -1,4 +1,14 @@
-# iterating through a list and printing the elements recursively
+# recursive number list
+def rec_increment(p):
+    if len(p) == 0:
+        return ""                    
+    else:      
+        print(p[0], end=" ")                 
+        return rec_increment(p[1:])  
+
+print(rec_increment([100, 20, 18, 12, 5, 5]))
+
+# iterating through a list and printing the elements 
 def num_list(numbers):
 	for index in range(0,len(numbers)):
 		return numbers[index:]
